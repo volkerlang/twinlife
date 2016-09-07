b6411d6202945970d85ec5b18f6c646f31ed84a2
Marcel Hebing <marcel@ddionrails.org>
2016-09-07 11:08:13 +0200

    Update fill_ddionrails.py, new location for xml import files

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -3,81 +3,13 @@ import pandas as pd
 
 sys.path.append(os.path.expanduser("~/github/ddi.py/"))
 
-from ddi.onrails.repos import merge_instruments
-
-def lower_all_names(x):
-    def lower_x(x):
-        try:
-            return x.lower()
-        except:
-            return x
-    names = [key for key in x.keys() if "_name" in key]
-    x.ix[ : , names] = x.ix[ : , names].applymap(lower_x)
-
-def answers():
-    x = pd.read_csv("metadata/answers.csv")
-    x.rename(columns={
-        "study":"study_name",
-        "questionnaire":"questionnaire_name",
-    }, inplace=True)
-    x["answer_list_name"] = x["answer_list"]
-    lower_all_names(x)
-    x.to_csv("ddionrails/answers.csv", index=False)
-
-def questions():
-    x = pd.read_csv("metadata/questions.csv")
-    x.rename(columns={
-        "study":"study_name",
-        "questionnaire":"questionnaire_name",
-        "question":"question_name",
-    }, inplace=True)
-    x["answer_list_name"] = x["answer_list"]
-    x["item_name"] = x["item"]
-    lower_all_names(x)
-    x.to_csv("ddionrails/questions.csv", index=False)
-
-def datasets():
-    x = pd.read_csv("metadata/logical_datasets.csv")
-    x.rename(columns={
-        "study":"study_name",
-        "dataset":"dataset_name",
-        "period":"period_name",
-        "analysis_unit":"analysis_unit_name",
-        "conceptual_dataset":"conceptual_dataset_name",
-    }, inplace=True)
-    lower_all_names(x)
-    x.to_csv("ddionrails/datasets.csv", index=False)
-
-def variables():
-    x = pd.read_csv("metadata/logical_variables.csv")
-    x.rename(columns={
-        "study":"study_name",
-        "dataset":"dataset_name",
-        "variable":"variable_name",
-        "concept":"concept_name",
-    }, inplace=True)
-    valid = x.ix[ : , ("study_name", "dataset_name", "variable_name")].duplicated() == False
-    x = x.ix[valid]
-    lower_all_names(x)
-    x.to_csv("ddionrails/variables.csv", index=False)
-
-def study():
-    os.system("cp metadata/study.md ddionrails")
-
-def r2ddi():
-    os.system("""
-        mkdir -p ddionrails/r2ddi/v1
-        cp -r r2ddi/v1/en ddionrails/r2ddi/v1
-    """)
+from ddi.onrails.repos import merge_instruments, dor1, copy
 
 def main():
-    study()
-    #datasets()
-    variables()
-    #questions()
-    #answers()
+    copy.study()
+    dor1.variables()
     merge_instruments.main()
-    r2ddi()
+    copy.r2ddi("r2ddi/v1/en", "ddionrails/r2ddi/v1")
 
 if __name__ == "__main__":
     main()
