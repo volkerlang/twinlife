002c5b24663966e7085e78a93923bbcac19342ec
mhebing <marcel@ddionrails.org>
2016-09-20 15:24:28 +0200

    Update twinlife to datasets.json

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 sys.path.append(os.path.expanduser("~/github/ddi.py/"))
 
-from ddi.onrails.repos import merge_instruments, dor1, copy
+from ddi.onrails.repos import merge_instruments, dor1, copy, convert_r2ddi
 
 def main():
     copy.study()
     dor1.variables()
     merge_instruments.main()
-    copy.r2ddi("r2ddi/v1/en", "ddionrails/r2ddi/v1")
+    convert_r2ddi.Parser(version="v1").write_json()
 
 if __name__ == "__main__":
     main()
