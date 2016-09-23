88e77676d6c1bca2c4c449ada5bdbc4edefeaecb
mhebing <marcel@ddionrails.org>
2016-09-23 10:12:15 +0200

    Include datasets.csv in import

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -8,6 +8,7 @@ from ddi.onrails.repos import merge_instruments, dor1, copy, convert_r2ddi
 def main():
     copy.study()
     dor1.variables()
+    copy.f("datasets.csv")
     merge_instruments.main()
     convert_r2ddi.Parser(version="v1").write_json()
 
